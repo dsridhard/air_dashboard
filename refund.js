2a997ed23cde4a3f2ff7b4ca8289f446a0ad2402
Yugal Sharma <yugalsharma@irctc.local>
2024-05-27 17:25:28 +0530

    Refund Heading

--- refund.js
+++ refund.js
@@ -4,8 +4,38 @@ const connectToOracle = require("./dbConn");
 router.get("/", async (req, res) => {
   try {
     const connection = await connectToOracle();
-    const result = await connection.execute("SELECT * FROM IR_REFUND");
+    const result = await connection.execute("SELECT  OID, REFUNDSEQID, STORE_ID, CREATION_TIME, STATUS, DELETED, LAST_MOD_TIME, TRANSACTIONID, REFUND_DATE, REFUND_AMOUNT, REFUND_STATUS, NOOFPASSENGERS, SETTLEMENT_ID, PAYMENT_GATEWAY_NAME, RECEIPT_NUMBER, ACTUAL_REFUND_DATE, DEFAULT_CREATION_TIME FROM IR_REFUND WHERE ROWNUM <= 50");
     await connection.close();
+
+    const columnHeadings = [
+      "ID",
+      "Refundseqid",
+      "Store id",
+      "Creation Time",
+      "Status",
+      "Deleted",
+      "Last Mod Time",
+      "TransactionId",
+      "Refund Date",
+      "Refund Amount",
+      "Refund Status",
+      "Number Of Passangers",
+      "Settlement Id",
+      "Payment Gateway Name",
+      "Receipt Number",
+      "Actual Refund Date",
+      "Default Creation Time"
+    ];
+
+    const rowsWithHeadings = result.rows.map(row => {
+      const rowData = {};
+      row.forEach((value, index) => {
+        rowData[columnHeadings[index]] = value;
+      });
+      return rowData;
+    });
+
+    res.json(rowsWithHeadings);
 
     res.json(result.rows);
   } catch (err) {
