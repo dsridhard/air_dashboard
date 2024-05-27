3d0b2fe28cd07b41d2620aec9e610da2fb3301ec
Yugal Sharma <yugalsharma@irctc.local>
2024-05-27 15:04:01 +0530

    Recon updated

--- booking_recon-data.js
+++ booking_recon-data.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const connectToOracle = require("./dbConn");
+
 router.get("/", async (req, res) => {
   try {
     const connection = await connectToOracle();
-    const result = await connection.execute("SELECT * FROM booking_recon_data WHERE ROWNUM <= 50");
+    const result = await connection.execute("SELECT TRANSACTION_ID, AIRLINE_PNR, AIRLINE, BOOKING_STATUS, FARE_AMOUNTS, SEAT_AMOUNT, MEAL_AMOUNT, BAGGAGE_AMOUNT, IRCTC_CHARGES, CREATION_DATE, BOOKING_DATE, PAYMENT_GATEWAY_NAME, PAYMENT_GATEWAY_ID, TICKET_NO FROM booking_recon_data WHERE ROWNUM <= 50");
     await connection.close();
 
     res.json(result.rows);
@@ -13,4 +14,5 @@ router.get("/", async (req, res) => {
     res.status(500).json({ error: "Internal server error" });
   }
 });
+
 module.exports = router;
